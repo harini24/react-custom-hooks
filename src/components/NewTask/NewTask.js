1fdd935a839f50eb23d6dea33330c1cdc633f8b8
harini <harinisiva1997@gmail.com>
2021-05-27 00:12:57 +0530

    using custom hook in more component

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,41 +1,30 @@
 import { useState } from 'react';
-
+import useHttp from '../../hooks/use-httpReq'
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
 
+
+  const { isLoading, error, sendReq: sendTasksReq } = useHttp()
+
+  const craeteTasks = (taskText,obj) => {
+    const generatedId = obj.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  }
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-burger-builder-29b01-default-rtdb.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
+    sendTasksReq({
+      url: 'https://react-burger-builder-29b01-default-rtdb.firebaseio.com//tasks.json',
+      method: 'POST',
+      body: { text: taskText },
+      headers: {
+        'Content-Type': 'application/json',
       }
+    }, craeteTasks.bind(this,taskText));
 
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
   };
 
   return (
