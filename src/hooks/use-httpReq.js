9c8c85c275dcbefa600face9352b5ae9e5af2657
harini <harinisiva1997@gmail.com>
2021-05-26 23:27:27 +0530

    building a cstom http hook

---
+++ src/hooks/use-httpReq.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+const useHttpRequest = (requestConfig,applyData) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const sendReq = async (taskText) => {
+        setIsLoading(true);
+        setError(null);
+        try {
+            const response = await fetch(
+                requestConfig.url, {
+                method: requestConfig.method,
+                headers: requestConfig.headers,
+                body: json.stringify(requestConfig.body)
+            });
+
+            if (!response.ok) {
+                throw new Error('Request failed!');
+            }
+
+            const data = await response.json();
+
+            applyData(data)
+        } catch (err) {
+            setError(err.message || 'Something went wrong!');
+        }
+        setIsLoading(false);
+    };
+
+}
+export default useHttpRequest
